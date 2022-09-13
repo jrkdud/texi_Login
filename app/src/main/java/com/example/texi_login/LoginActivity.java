c7f5ce06583d51ecd3c8f64aed4e5dbca17edd1f
jrkdud <newgy28@naver.com>
2022-09-13 19:55:56 +0900

    message3

--- app/src/main/java/com/example/texi_login/LoginActivity.java
+++ app/src/main/java/com/example/texi_login/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.texi_login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button button = findViewById(R.id.login_signup_bt);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
